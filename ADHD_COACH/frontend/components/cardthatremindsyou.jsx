f810f57dc3ef6df20b5020e3db53f7e4914771da
Shabdi Satyal <satyalshabdi2023@gmail.com>
2026-08-04 11:04:16 +0545

    Shuffle function added

--- ADHD_COACH/frontend/components/cardthatremindsyou.jsx
+++ ADHD_COACH/frontend/components/cardthatremindsyou.jsx
@@ -21,3 +21,43 @@ export function useFolderShuffle(images, startIndex = 0) {
     shuffle,
   };
 }
+
+
+
+export function Card({ images, label }) {
+  const { currentImage, currentIndex, shuffle } = useFolderShuffle(images);
+
+  return (
+    <div className='ImageShufflerContainer border-4 border-black rounded-none' onClick={shuffle}>
+      <img src={currentImage} alt={label} />
+      <div className="dots">
+        {images.map((_, i) => (
+          <span
+            key={i}
+            className={i === currentIndex ? 'dot dot-active' : 'dot'}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+
+export const imgfolder = [
+{
+label:'motivation', 
+images: [
+  'https://i.pinimg.com/736x/0f/18/a3/0f18a36ecf97385615a384af03e6a514.jpg',
+  'https://i.pinimg.com/736x/0b/a1/6a/0ba16a1be3ae8070a56bbeb7da769fa7.jpg',
+  'https://i.pinimg.com/736x/29/b8/01/29b8011472227557eeaec20a84b3f5ee.jpg',
+
+]
+
+}
+
+
+
+
+
+
+]
--- ADHD_COACH/src/App.jsx
+++ ADHD_COACH/src/App.jsx
@@ -1,31 +1,26 @@
 import './App.css';
+import { Card } from '../frontend/components/cardthatremindsyou';
+import { imgfolder } from '../frontend/components/cardthatremindsyou';
 
+class User {
+  constructor(name) {
+    this.user = name;
+  }
+
+  present() {
+    return 'Hi ' + this.user + ', Welcome!';
+  }
+}
 
 function App() {
-  const MyUser= new User("Shea");
-  //I want to later add a shuffling feature in this 
-  return ( 
+  const MyUser = new User("Shea");
+  return (
     <div className="App">
-
-      <h1> CAREMEL </h1>
+      <h1>CAREMEooL</h1>
       <h3 className="introline">{MyUser.present()}</h3>
-
+      <Card images={imgfolder[0].images} label={imgfolder[0].label} />
     </div>
-
   );
 }
 
-class User {
-        constructor(name) {
-          this.user=name;
-        }
-
-        present() {
-          return 'Hi '+ this.user + 'Welcome!';
-          return 'The user is '+ this.user
-        }
-      }
-
-
-    
-export default App; 
+export default App;
