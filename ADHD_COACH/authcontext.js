b649cdc6cdf67cc55fb41bff5b992e44b2c6bba9
Shabdi Satyal <satyalshabdi2023@gmail.com>
2026-08-24 12:48:04 +0545

    check1

--- ADHD_COACH/authcontext.js
+++ ADHD_COACH/authcontext.js
@@ -1,31 +1,31 @@
 
-import React, { createContext, useContext, useEffect, useState } from "react";
-import { supabase } from "./supabaseClient";
+// import React, { createContext, useContext, useEffect, useState } from "react";
+// import { supabase } from "./supabaseClient";
 
-const AuthContext = createContext(null);
+// const AuthContext = createContext(null);
 
-export const AuthProvider = ({ children }) => {
-  const [session, setSession] = useState(undefined); // undefined as in "still checking"
+// export const AuthProvider = ({ children }) => {
+//   const [session, setSession] = useState(undefined); // undefined as in "still checking"
 
-  useEffect(() => {
-   //this checks on load
-    supabase.auth.getSession().then(({ data }) => {
-      setSession(data.session);
-    });
+//   useEffect(() => {
+//    //this checks on load
+//     supabase.auth.getSession().then(({ data }) => {
+//       setSession(data.session);
+//     });
 
    
-    const { data: listener } = supabase.auth.onAuthStateChange((_event, newSession) => {
-      setSession(newSession);
-    });
-//clean
-    return () => listener.subscription.unsubscribe();
-  }, []);
+//     const { data: listener } = supabase.auth.onAuthStateChange((_event, newSession) => {
+//       setSession(newSession);
+//     });
+// //clean
+//     return () => listener.subscription.unsubscribe();
+//   }, []);
 
-  return (
-    <AuthContext.Provider value={{ session }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
+//   return (
+//     <AuthContext.Provider value={{ session }}>
+//       {children}
+//     </AuthContext.Provider>
+//   );
+// };
 
-export const useAuth = () => useContext(AuthContext);
+// export const useAuth = () => useContext(AuthContext);
