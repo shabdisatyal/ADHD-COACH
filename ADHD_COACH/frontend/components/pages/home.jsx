3891e0382acecee78c3c29bb251748797244592b
Shabdi <satyalshabdi2023@gmail.com>
2026-09-13 13:50:12 +0545

    sticker overlay

--- ADHD_COACH/frontend/components/pages/home.jsx
+++ ADHD_COACH/frontend/components/pages/home.jsx
@@ -1,17 +1,15 @@
 import CardPageTwo from '../cardpagetwo';
 import { Card } from '../cardthatremindsyou';
 import { textfolder } from '../cardthatremindsyou';
-import { StickerPlayground } from '../stickerplayground';
+// import { StickerPlayground } from '../stickerplayground';
 
 export function Home() {
   return (
     <div className="relative">
       <Card text={textfolder[0].text} label={textfolder[0].label} />
-      
+      {/* <StickerPlayground /> */}
       <CardPageTwo />
-      <div className="spotlight">
-        <div className=""></div>
-      </div>
+    
     </div>
   );
 }
