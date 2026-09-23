adf03881e70907c652909012934d69bb71adc036
Shabdi <satyalshabdi2023@gmail.com>
2026-09-23 23:09:31 +0545

    Footer and notice

--- ADHD_COACH/frontend/components/pages/Footer.jsx
+++ ADHD_COACH/frontend/components/pages/Footer.jsx
@@ -1,4 +1,4 @@
-import { Instagram, Linkedin } from "lucide-react";
+// import {  Linkedin } from "lucide-react";
 
 
 export function Footer() {
@@ -6,14 +6,14 @@ export function Footer() {
     <footer className="FooterContent w-full bg-[##BBD5DA] text-black">
       <div className="mx-auto flex max-w-6xl flex-col items-center justify-between gap-4 px-6 py-6 md:flex-row">
         <p className="text-sm">
-          © {new Date().getFullYear()} Your Name. All rights reserved.
+          © {new Date().getFullYear()} CaremelFlow. All rights reserved.
         </p>
 
         <a href="public/customerprotection/privacy-policy.html" className="text-sm underline hover:opacity-70">
           Privacy Policy
         </a>
 
-        <div className="flex items-center gap-4">
+        {/* <div className="flex items-center gap-4">
           
           <a  href="https://www.instagram.com/yourhandle"
             target="_blank"
@@ -34,7 +34,7 @@ export function Footer() {
           >
             <Linkedin size={22} />
           </a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
