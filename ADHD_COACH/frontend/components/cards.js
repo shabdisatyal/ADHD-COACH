fe108fe7141fd6c34c2f5b671ef5f8ab97ff68a5
Shabdi <satyalshabdi2023@gmail.com>
2026-09-26 17:57:01 +0545

    sm2

--- ADHD_COACH/frontend/components/cards.js
+++ ADHD_COACH/frontend/components/cards.js
@@ -1,2 +1,8 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { reviewCard } from "./sm2";
+
+//to get decks so that manage.jsx can show what decks are even there to click
+//to add decks-yeah
+//to insert new cards into decks
+//to get card that already exist from deck
+//
